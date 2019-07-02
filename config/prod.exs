c14126ba282940e72d636d9d76bbf19d1018c811
Steven Anderson <steve@whilefalse.net>
2019-07-02 23:09:43 +0100

    Heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :sleep_chart, SleepChartWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [scheme: "https", host: "stormy-scrubland-18954", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
