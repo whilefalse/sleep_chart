e756b943a0fb112afd8a92a2faf82c8dd69e609a
Steven Anderson <steve@whilefalse.net>
2019-07-03 00:02:46 +0100

    Removed some more stuff that was stopping deploy

--- lib/sleep_chart_web/endpoint.ex
+++ lib/sleep_chart_web/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule SleepChartWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :sleep_chart
 
-  socket "/socket", SleepChartWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
