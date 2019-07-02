ea35a3cbff7ec9ed64ccddf13b7f5674f68d6454
Steven Anderson <steve@whilefalse.net>
2019-07-02 23:40:41 +0100

    Internationalise the custom error message.

--- lib/sleep_chart/sleeps/sleep.ex
+++ lib/sleep_chart/sleeps/sleep.ex
@@ -1,6 +1,7 @@
 defmodule SleepChart.Sleeps.Sleep do
   use Ecto.Schema
   import Ecto.Changeset
+  import SleepChartWeb.Gettext
 
   schema "sleeps" do
     field :date, :date
@@ -14,7 +15,8 @@ defmodule SleepChart.Sleeps.Sleep do
     sleep
     |> cast(attrs, [:date, :slept])
     |> validate_required([:date, :slept])
-    |> unique_constraint(:date, [message: "You've already recorded a sleep for this date."])
+    |> unique_constraint(:date, [
+      message: dgettext("errors", "You've already recorded a sleep for this date.")])
   end
 
 end
