482e603dd5496d872424ac85858a95f4dc67023b
Steven Anderson <steven_anderson@waters.com>
2019-07-03 08:42:31 +0100

    Move heroku deploy to a nicer domain

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :sleep_chart, SleepChartWeb.Endpoint,
-  url: [scheme: "https", host: "stormy-scrubland-18954", port: 443],
+  url: [scheme: "https", host: "sleepchart", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
